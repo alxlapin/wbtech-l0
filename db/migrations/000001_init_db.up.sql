bd6d575d99f4c652132cf3758550136a66be8513
Alexei Lapin <lapin.alx@gmail.com>
2025-06-29 20:01:42 +0300

    update init_db migration:
    - remove shardkey and oof_shard from orders table
    - add some check constraints

--- db/migrations/000001_init_db.up.sql
+++ db/migrations/000001_init_db.up.sql
@@ -7,10 +7,8 @@ CREATE TABLE orders
     internal_signature varchar(255) NOT NULL,
     customer_id        varchar(255) NOT NULL,
     delivery_service   varchar(255) NOT NULL,
-    shardkey           varchar(255) NOT NULL,
     sm_id              integer      NOT NULL,
-    date_created       timestamp(0) NOT NULL,
-    oof_shard          varchar(255) NOT NULL
+    date_created       timestamp(0) NOT NULL
 );
 
 CREATE TABLE deliveries
@@ -37,9 +35,10 @@ CREATE TABLE payments
     amount        integer      NOT NULL,
     payment_dt    timestamp(0) NOT NULL,
     bank          varchar(255) NOT NULL,
-    delivery_cost integer      NOT NULL,
-    goods_total   integer      NOT NULL,
-    custom_fee    integer      NOT NULL
+    delivery_cost integer      NOT NULL CHECK ( delivery_cost >= 0 ),
+    goods_total   integer      NOT NULL CHECK ( goods_total > 0 ),
+    custom_fee    integer      NOT NULL CHECK ( custom_fee >= 0 ),
+    CHECK ( amount = delivery_cost + goods_total + custom_fee )
 );
 
 CREATE TABLE items
@@ -48,10 +47,10 @@ CREATE TABLE items
     order_uid    varchar(255) NOT NULL REFERENCES orders (order_uid),
     chrt_id      integer      NOT NULL,
     track_number varchar(255) NOT NULL,
-    price        integer      NOT NULL,
+    price        integer      NOT NULL CHECK ( price > 0 ),
     rid          varchar(255) NOT NULL,
     name         varchar(255) NOT NULL,
-    sale         integer      NOT NULL,
+    sale         integer      NOT NULL CHECK ( sale >= 0 AND sale < 100 ),
     size         varchar(255) NOT NULL,
     total_price  integer      NOT NULL,
     nm_id        integer      NOT NULL,
